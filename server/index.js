5ca61335eae2c5c76c3038c671a308e2023a27f0
John Desrosiers <john.desro@gmail.com>
2020-01-18 16:07:54 -0800

    Add auth configuration to server
    This commit adds configuration for providing authentication routes
    and middleware. Passport is used with a JWT strategy.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,5 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-app.set("port", process.env.PORT || 3001);
-
-// Serve static assets in production only
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+require("dotenv").config();
+const { app } = require("./app");
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`);
